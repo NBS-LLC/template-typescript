218213f271ad550a9ad8dd4be993419db8de215b
Nick <nick@networkbasedsolutions.com>
2020-08-28 19:01:57 -0700

    Added webpack for easier bundling and distribution.
    * Included the html-webpack-plugin dev-dependency to make generating the html entry point easier.

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './build/app.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Template - Typescript w/ Webpack',
+        }),
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
